fcbc4e1c423eee89a10940f611a82bb322fce226
Kyour.cn <kyour@outlook.com>
2023-10-31 21:49:10 +0800

    feat(app): 新增app组件

---
+++ components/app/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <view class="pui-app" :style="style">
+        <slot name="default"></slot>
+    </view>
+</template>
+
+<script setup>
+
+
+import {usePetalUiStore} from "../../stores/petal-ui";
+import {computed} from "vue";
+
+const puiStore = usePetalUiStore()
+
+const style = computed(() => {
+    return {
+        background: puiStore.theme['bg-page'],
+    }
+})
+
+</script>
+
+<style scoped>
+.pui-app {
+    height: 100%;
+}
+</style>
