61b404407cd7b5ac6353a5e8a1827e9ff3257abf
Kyour.cn <kyour@outlook.com>
2023-11-01 14:11:56 +0800

    style(tabbar): 新增tabbar组件

---
+++ components/tabbar-item/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <view class="petal-tabbar-item">
+
+    </view>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+.petal-tabbar-item {
+    border: #ced1d5 1px solid;
+    display: flex;
+    flex: 1;
+}
+
+</style>
---
+++ components/tabbar/index.vue
@@ -0,0 +1,68 @@
+<template>
+    <view
+        v-if="props.placeholder"
+        class="petal-navbar-placeholder"
+    />
+    <view
+        class="petal-navbar"
+        :style="{
+            background: style.bgColor,
+            zIndex: props.zIndex
+        }"
+    >
+        <slot name="default">
+            None
+        </slot>
+    </view>
+</template>
+
+<script setup>
+
+import {computed, defineProps} from "vue";
+
+import {usePetalUiStore} from "../../stores/petal-ui";
+
+const puiStore = usePetalUiStore()
+
+const props = defineProps({
+    title: {
+        type: String,
+        default: 'Title'
+    },
+    placeholder: {
+        type: Boolean,
+        default: true
+    },
+    zIndex: {
+        type: Number,
+        default: 999
+    },
+})
+
+const style = computed(() => {
+    return {
+        titleColor: puiStore.theme['title'],
+        bgColor: puiStore.theme['bg-page']
+    }
+})
+
+</script>
+
+<style scoped>
+
+.petal-navbar-placeholder {
+    height: 80rpx;
+    width: 100%;
+}
+
+.petal-navbar {
+    height: 80rpx;
+    display: flex;
+    bottom: 0;
+    font-weight: 700;
+    width: 100%;
+    position: fixed;
+    z-index: 999;
+}
+
+</style>
