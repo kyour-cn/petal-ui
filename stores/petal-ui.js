f2648e24c3f9f999f7e743aca6cd1b751d32524c
Kyour.cn <kyour@outlook.com>
2023-11-01 14:12:47 +0800

    style: 主题配置完善

--- stores/petal-ui.js
+++ stores/petal-ui.js
@@ -7,9 +7,13 @@ let defaultTheme = {
 	// 行为相关颜色
 	'primary': '#007DFF',
 	'success': '#2ECC71',
+	'warning': '#F0AD4E',
+	'error': '#DD524D',
 
 	// 文字和其他辅助色
 	'title': '#181818',
+	'label': '#B2B2B2',
+	'subtitle': '#7F7F7F',
 
 	'bg-page': '#F3F4F6',
 	'bg-body': '#FFFFFF',
@@ -18,6 +22,8 @@ let defaultTheme = {
 
 let darkTheme = {
 	...defaultTheme,
+
+	'title': '#DCDDDF',
 
 	'bg-page': '#000000',
 	'bg-body': '#212224',
@@ -35,12 +41,12 @@ export const usePetalUiStore = defineStore('petal_ui', {
 	actions: {
 		setTheme(values, isDark = false) {
 			//TODO: 这里设置主题颜色后getters的theme并不会变化
-			if(isDark) {
-				for(let i in values) {
+			if (isDark) {
+				for (let i in values) {
 					darkTheme[i] = values[i];
 				}
-			}else{
-				for(let i in values) {
+			} else {
+				for (let i in values) {
 					defaultTheme[i] = values[i];
 				}
 			}
