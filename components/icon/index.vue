3f2c60f116a4f2db0f6914c3f48bf99c1fd8025b
Kyour.cn <kyour@outlook.com>
2023-11-02 18:04:05 +0800

    feat: 新增icon组件

---
+++ components/icon/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <text :class="'petal-icon-'+props.name"></text>
+</template>
+
+<script setup>
+
+import {defineProps} from "vue";
+
+const props = defineProps({
+    name: {
+        type: String,
+        default: ''
+    },
+})
+
+</script>
