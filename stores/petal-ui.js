bd9abfba78b6db99183f4ac52e18035f7d1f195e
kyour-cn <kyour@outlook.com>
2023-11-03 23:56:20 +0800

    perf: 主题配置独立文件

--- stores/petal-ui.js
+++ stores/petal-ui.js
@@ -1,36 +1,10 @@
 import {defineStore} from 'pinia';
 
+import {defaultTheme, darkTheme} from '../lib/theme'
+
 let isDark = false;
 
 const windowInfo = uni.getWindowInfo()
-
-let defaultTheme = {
-
-	// 行为相关颜色
-	'primary': '#007DFF',
-	'success': '#2ECC71',
-	'warning': '#F0AD4E',
-	'error': '#DD524D',
-
-	// 文字和其他辅助色
-	'title': '#181818',
-	'label': '#B2B2B2',
-	'subtitle': '#7F7F7F',
-
-	'bg-page': '#F3F4F6',
-	'bg-body': '#FFFFFF',
-	'divider': '#CECECE',
-}
-
-let darkTheme = {
-	...defaultTheme,
-
-	'title': '#DCDDDF',
-
-	'bg-page': '#000000',
-	'bg-body': '#212224',
-	'divider': '#4C4D4F',
-}
 
 export const usePetalUiStore = defineStore('petal_ui', {
 	state: () => {
