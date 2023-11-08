19fc8371de2fd9e91d9be4bbc69840f8f883586a
Kyour.cn <kyour@outlook.com>
2023-11-08 14:29:28 +0800

    style(slider): 滑动条组件开发

--- components/slider/index.vue
+++ components/slider/index.vue
@@ -1,11 +1,22 @@
 <template>
-    <slider
-        :model="value"
-        :step="props.step"
-        @changing="sliderChangeing"
-        @change="sliderChange"
-        style="height: 20rpx"
-    />
+<view class="petal-slider">
+    <view class="petal-slider-inner">
+        <view class="petal-slider-bar" :style="{width: `${value.value}%`}">
+            <view class="petal-slider-bar-inner"></view>
+            <view class="petal-slider-bar-outer"></view>
+        </view>
+        <view
+            class="petal-slider-handle"
+            :style="{left: `${value.value}%`}"
+            @touchstart="touchStart"
+            @touchmove.native="touchMove"
+            @touchend="touchEnd"
+        >
+            <view class="petal-slider-handle-inner"></view>
+        </view>
+    </view>
+</view>
+
 </template>
 
 <script setup>
@@ -35,24 +46,67 @@ const value = computed({
     set: (value) => emits('update:modelValue', value)
 })
 
+const touchStart = (e) => {
+    console.log(e)
+}
+const touchMove = (e) => {
+
+    console.log('横向移动距离：' + e.deltaX);
+
+}
+const touchEnd = () => {
+}
+
+
 const sliderChangeing = (e) => {
-    value.value = e.detail.value;
-    emits('changing', e.detail.value)
+    console.log(e)
+    // value.value = e.detail.value;
+    // emits('changing', e.detail.value)
 };
 
 const sliderChange = (e) => {
-    value.value = e.detail.value;
-    emits('change', e.detail.value)
+    console.log(e)
+    // value.value = e.detail.value;
+    // emits('change', e.detail.value)
 };
 
 </script>
 
 <style scoped>
-:deep(.uni-slider-wrapper) .uni-slider-handle-wrapper {
+.petal-slider {
     height: 20rpx;
+    margin: 10rpx;
+    background: #ccc;
+    position: relative;
+    border-radius: 10rpx;
 }
-
-:deep(.uni-slider-wrapper) .uni-slider-bar {
-    height: 10px;
+.petal-slider-inner {
+    height: 100%;
+    width: 100%;
+    position: relative;
+}
+.petal-slider-bar {
+    height: 100%;
+    background: #007aff;
+    position: absolute;
+    border-radius: 10rpx;
+}
+.petal-slider-bar-inner {
+    height: 100%;
+    background: #fff;
+}
+.petal-slider-bar-outer {
+    height: 100%;
+    background: #007aff;
+}
+.petal-slider-handle {
+    width: 40rpx;
+    height: 40rpx;
+    background: #fff;
+    border-radius: 20rpx;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    z-index: 1;
 }
 </style>
