dc94f5a3a9d91fc2c3afeb951a3fb879f0170bf9
Kyour.cn <kyour@outlook.com>
2023-11-11 17:56:09 +0800

    style(popup): 完善popup样式

--- components/popup/index.vue
+++ components/popup/index.vue
@@ -14,9 +14,7 @@
             @click.stop=""
         >
             <view class="petal-popup-body">
-                <slot name="default">
-                    {{style}}
-                </slot>
+                <slot name="default"/>
             </view>
         </view>
     </view>
@@ -26,24 +24,39 @@
 
 import {computed, ref, watch} from "vue";
 import {usePetalUiStore} from "../../stores/petal-ui";
+import {normalizePadding2} from "../../lib/utils.js"
 
 const puiStore = usePetalUiStore()
 
 const props = defineProps({
-    // 弹窗是否显示
+    // 弹出层是否显示
     modelValue: {
         type: Boolean,
         default: 0
     },
-    // 弹窗高度
+    // 弹出层高度
     height: {
         type: Number,
         default: 500
     },
-    // 点击遮罩层是否关闭弹窗
+    // 弹出层宽度 -1表示自适应
+    width: {
+        type: Number,
+        default: -1
+    },
+    // 点击遮罩层是否关闭弹出层
     closeOnClickOverlay: {
         type: Boolean,
         default: true
+    },
+    // 弹出位置
+    position: {
+        type: String,
+        default: 'center'
+    },
+    padding: {
+        type: [Number, Array],
+        default: 20
     },
     zIndex: {
         type: Number,
@@ -70,21 +83,23 @@ watch(value, () => {
 })
 
 // 动画位移
-let offset = ref(value.value? 0 : -props.height)
+let offset = ref(value.value ? 0 : -props.height)
 
+// 移入移出位移动画
 const move = () => {
     setTimeout(() => {
         offset.value = value.value ? offset.value + 10 : offset.value - 10
         if (value.value && offset.value >= 0) {
             offset.value = 0
-        }else if(!value.value && offset.value <= -props.height) {
+        } else if (!value.value && offset.value <= -props.height) {
             offset.value = -props.height
-        }else{
+        } else {
             move()
         }
     }, 5)
 }
 
+// 点击遮罩层
 const onClickOverlay = () => {
     if (props.closeOnClickOverlay) {
         value.value = false
@@ -92,13 +107,51 @@ const onClickOverlay = () => {
     emits('clickOverlay')
 }
 
-// 位置
 const style = computed(() => {
-    return {
+    const obj = {
         height: props.height + 'rpx',
-        bottom: offset.value + 'rpx',
         background: puiStore.theme['bg-popup']
     }
+
+    // 如果是中间弹出，使用透明度渐入渐出
+    if (props.position === 'center') {
+        obj.opacity = 1 - Math.abs(offset.value) / props.height
+    }
+
+    // 边距
+    let padding = props.padding
+    if (!Array.isArray(props.padding)) {
+        padding = [padding]
+    }
+    padding = normalizePadding2(padding)
+
+    // 宽度
+    if (props.width > 0) {
+        obj.width = props.width + 'rpx'
+    } else {
+        obj.width = 'calc(100% - ' + (padding[1] + padding[3]) + 'rpx)'
+    }
+
+    // 适配不同位置的样式
+    switch (props.position) {
+        case 'top':
+            obj.top = offset.value + 'rpx'
+            obj.borderRadius = '0 0 40rpx 40rpx'
+            padding[0] = 0
+            break;
+        case 'bottom':
+            obj.bottom = offset.value + 'rpx'
+            obj.borderRadius = '40rpx 40rpx 0 0'
+            padding[2] = 0
+            break;
+        case 'center':
+        default:
+            obj.top = 'calc(50% - ' + (props.height / 2) + 'rpx)'
+            obj.borderRadius = '40rpx'
+            break;
+    }
+    obj.margin = padding.join('rpx ') + 'rpx'
+    return obj
 })
 
 </script>
@@ -110,13 +163,14 @@ const style = computed(() => {
     top: 0;
     width: 100%;
     height: 100%;
+    display: flex;
 }
 
 .petal-popup {
-    border-radius:  40rpx 40rpx 0 0;
     position: fixed;
     width: 100%;
 }
+
 .petal-popup-body {
     padding: 20rpx;
 }
