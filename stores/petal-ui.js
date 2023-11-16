5d60ad0f8c4070d0a7571d9d95221fc54e16750f
Kyour.cn <kyour@outlook.com>
2023-11-16 11:23:38 +0800

    style: 优化js代码缩进

--- stores/petal-ui.js
+++ stores/petal-ui.js
@@ -5,43 +5,43 @@ import {defaultTheme, darkTheme} from '../lib/theme'
 let isDark = false;
 
 let windowInfo = null,
-	systemInfo = null;
+    systemInfo = null;
 
 export const usePetalUiStore = defineStore('petal_ui', {
-	state: () => {
-		if (windowInfo === null) {
-			windowInfo = uni.getWindowInfo()
-			systemInfo = uni.getSystemInfoSync()
-		}
-		return {
-			dark: isDark,
+    state: () => {
+        if (windowInfo === null) {
+            windowInfo = uni.getWindowInfo()
+            systemInfo = uni.getSystemInfoSync()
+        }
+        return {
+            dark: isDark,
 
-			windowInfo,
-			systemInfo,
+            windowInfo,
+            systemInfo,
 
-			screenWidth: windowInfo.screenWidth,
-			screenHeight: windowInfo.screenHeight,
-			safeAreaInsets: windowInfo.safeAreaInsets,
+            screenWidth: windowInfo.screenWidth,
+            screenHeight: windowInfo.screenHeight,
+            safeAreaInsets: windowInfo.safeAreaInsets,
 
-		}
-	},
-	actions: {
-		setTheme(values, isDark = false) {
-			//TODO: 这里设置主题颜色后getters的theme并不会变化
-			if (isDark) {
-				for (let i in values) {
-					darkTheme[i] = values[i];
-				}
-			} else {
-				for (let i in values) {
-					defaultTheme[i] = values[i];
-				}
-			}
-		},
-	},
-	getters: {
-		theme: (state) => {
-			return state.dark ? darkTheme : defaultTheme;
-		}
-	}
+        }
+    },
+    actions: {
+        setTheme(values, isDark = false) {
+            //TODO: 这里设置主题颜色后getters的theme并不会变化
+            if (isDark) {
+                for (let i in values) {
+                    darkTheme[i] = values[i];
+                }
+            } else {
+                for (let i in values) {
+                    defaultTheme[i] = values[i];
+                }
+            }
+        },
+    },
+    getters: {
+        theme: (state) => {
+            return state.dark ? darkTheme : defaultTheme;
+        }
+    }
 });
