5d4651da39d4e80061a3dc6ee78d526e5d236a6c
kyour-cn <kyour@outlook.com>
2023-11-21 21:48:11 +0800

    perf(grid): 新宫格组件

---
+++ components/grid-item/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <view class="petal-grid-item">
+        <slot name="default"></slot>
+    </view>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.petal-grid-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
---
+++ components/grid/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <view
+        class="petal-grid"
+        :style="{
+            gridTemplateColumns: `repeat(${props.col}, 1fr)`
+        }"
+    >
+        <slot name="default"></slot>
+    </view>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    width: {
+        type: Number,
+        default: 0
+    },
+    col: {
+        type: Number,
+        default: 5
+    }
+})
+
+</script>
+
+<style scoped>
+
+.petal-grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+}
+
+</style>
