22bd305de85633f5ff0c2d52e2b753bdcad3741f
Kyour.cn <kyour@outlook.com>
2023-12-16 21:56:10 +0800

    feat(card): 新增卡片组件

---
+++ components/card/index.vue
@@ -0,0 +1,72 @@
+<template>
+    <view
+        class="petal-card"
+        :style="style"
+    >
+        <slot name="default" />
+    </view>
+</template>
+
+<script setup>
+
+import {computed} from "vue";
+import {usePetalUiStore} from "../../stores/petal-ui";
+
+const puiStore = usePetalUiStore()
+
+const props = defineProps({
+    background: {
+        type: String,
+        default: 'bg-body'
+    },
+    round: {
+        type: Number,
+        default: 10
+    },
+    width: {
+        type: Number,
+        default: -1
+    },
+    height: {
+        type: Number,
+        default: -1
+    },
+    padding: {
+        type: Number,
+        default: 20
+    },
+    margin: {
+        type: Number,
+        default: 20
+    },
+    _style: {
+        type: Object,
+        default: () => ({})
+    }
+})
+
+const style = computed(() => {
+
+    const obj = {
+        borderRadius: props.round + 'rpx',
+        background: puiStore.theme[props.background] || props.background,
+        padding: props.padding + 'rpx',
+        margin: props.margin + 'rpx',
+        ...props._style
+    }
+
+    if (props.width >= 0) {
+        obj.width = props.width + 'rpx'
+    }
+    if (props.height >= 0) {
+        obj.height = props.height + 'rpx'
+    }
+
+    return obj
+})
+
+</script>
+
+<style scoped>
+
+</style>
