e0140977222b25619f04475b4355f2e148f6eb30
Kyour.cn <kyour@outlook.com>
2024-01-20 17:31:37 +0800

    feat(tabs): 优化完善切换导航效果

---
+++ components/tabs/index.vue
@@ -0,0 +1,111 @@
+<script setup>
+import {computed} from "vue";
+import {usePetalUiStore} from "../../stores/petal-ui";
+
+const puiStore = usePetalUiStore()
+
+const props = defineProps({
+    modelValue: {
+        type: Number,
+        default: 0
+    },
+    list: {
+        type: Array,
+        default: () => []
+    },
+    key: {
+        type: String,
+        default: 'name'
+    }
+})
+
+const emits = defineEmits([
+    'update:modelValue',
+    'change'
+])
+
+// 生成随机ID
+const tabId = 'tabs_' + Math.floor(Math.random() * 10000)
+
+// TODO: 还需优化滚动位置
+const showIntoId = computed(() => {
+    if(active.value - props.list.length > props.list.length / 2) {
+        return tabId + (active.value + 3)
+    }
+    return tabId + (active.value - 3)
+})
+
+const onChange = (key) => {
+    active.value = key
+}
+
+const active = computed({
+    get: () => props.modelValue,
+    set: (value) => {
+        emits('update:modelValue', value)
+        emits('change', value)
+    }
+})
+
+const style = computed(() => {
+    return {
+        color: puiStore.theme['title'],
+    }
+})
+
+</script>
+
+<template>
+    <scroll-view
+        :scroll-x="true"
+        :show-scrollbar="false"
+        :scroll-with-animation="true"
+        :scroll-into-view="showIntoId"
+    >
+        <view class="petal-tabs" :style="style">
+            <view
+                v-for="(item, key) in list"
+                :id="tabId+ key"
+                :key="key"
+                :class="{
+                    'tab-item': true,
+                    'active': active === key
+                }"
+                @click="onChange(key)"
+            >
+                {{item?.[props.key]}}
+            </view>
+        </view>
+    </scroll-view>
+
+</template>
+
+<style scoped>
+
+.petal-tabs {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.petal-tabs .tab-item {
+    margin: 10rpx;
+    padding-bottom: 8rpx;
+    flex: 1;
+    white-space: nowrap;
+}
+
+.petal-tabs .active {
+    color: #0051ff;
+    border-bottom: 4rpx solid #0051ff;
+    padding-bottom: 4rpx;
+}
+
+scroll-view::-webkit-scrollbar {
+    display: none;
+    width: 0;
+    height: 0;
+    color: transparent;
+}
+
+</style>
