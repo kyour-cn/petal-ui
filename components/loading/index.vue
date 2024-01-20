96b8fcda3ba38160a760d2668f2d366549d3f68d
Kyour.cn <kyour@outlook.com>
2024-01-20 22:25:06 +0800

    fix(loading): 新增加载动画组件
    不好实现行星加载动画，暂时使用图片显示，来自华为商城

---
+++ components/loading/index.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import PuiIcon from "../icon"
+
+const props = defineProps({
+    size: {
+        type: [Number, String],
+        default: 50
+    },
+    src: {
+        type: String,
+        default: '/static/image/loading.webp'
+    }
+})
+
+</script>
+
+<template>
+    <PuiIcon :name="props.src" :size="props.size" />
+</template>
