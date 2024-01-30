c3bb15ae374ce5fd20a2fc08b62d08349cabf44b
Kyour.cn <kyour@outlook.com>
2024-01-30 17:57:00 +0800

    fix(popover): 新增气泡弹出框组件

---
+++ components/popover/index.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {usePetalUiStore} from "../../stores/petal-ui";
+const puiStore = usePetalUiStore()
+
+const props = defineProps({
+    show: Boolean,
+    disabled: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const emits = defineEmits([
+    'update:show'
+])
+
+</script>
+
+<template>
+
+    <view class="petal-popover">
+        <slot name="content" />
+    </view>
+
+</template>
+
+<style scoped>
+
+</style>
