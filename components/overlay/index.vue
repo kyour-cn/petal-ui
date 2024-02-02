b556a7faf4f335298d970c089760601fa219c89b
Kyour.cn <kyour@outlook.com>
2024-02-02 23:32:38 +0800

    feat(overlay): 新增遮罩组件

---
+++ components/overlay/index.vue
@@ -0,0 +1,50 @@
+<script setup>
+
+const props = defineProps({
+    show: {
+        type: Boolean,
+        default: false,
+    },
+    zIndex: {
+        type: Number,
+        default: 100
+    },
+    background: {
+        type: String,
+        default: 'rgba(0, 0, 0, 0.5)'
+    }
+});
+
+</script>
+
+<template>
+    <view
+        v-if="show"
+        class="petal-overlay"
+        :style="{
+            zIndex: zIndex,
+            background: background
+        }"
+        @touchmove.stop.prevent="() => {}"
+    >
+        <view class="petal-overlay-content">
+            <slot>
+            </slot>
+        </view>
+    </view>
+</template>
+
+<style scoped>
+
+.petal-overlay {
+    position: fixed;
+    display: flex;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+.petal-overlay-content {
+    margin: auto;
+}
+</style>
