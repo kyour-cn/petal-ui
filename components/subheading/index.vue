912f2ccc50f53ce19fe505d4ed5f7b0411b901e7
Kyour.cn <kyour@outlook.com>
2024-02-05 23:20:47 +0800

    feat(subheading): 新增子标题组件

---
+++ components/subheading/index.vue
@@ -0,0 +1,36 @@
+<script setup>
+// TODO: 待完善
+
+import {computed} from "vue";
+import {usePetalUiStore} from "../../stores/petal-ui";
+
+const puiStore = usePetalUiStore()
+
+const props = defineProps({
+    title: {
+        type: String,
+        default: ''
+    }
+})
+
+const style = computed(() => {
+    return {
+        color: puiStore.theme.subtitle
+    }
+})
+</script>
+
+<template>
+    <view class="petal-subheading" :style="style">
+        <view>
+            <text>{{props.title}}</text>
+        </view>
+    </view>
+</template>
+
+<style scoped>
+.petal-subheading {
+    margin: 20rpx 20rpx 0 40rpx;
+}
+
+</style>
