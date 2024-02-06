e7ae06a7e2d4445a85ab7427d29d696ab5be5e84
Kyour.cn <kyour@outlook.com>
2024-02-06 15:18:04 +0800

    perf(pinia): 状态管理库改为组合式API写法

--- stores/petal-ui.js
+++ stores/petal-ui.js
@@ -1,45 +1,30 @@
 import {defineStore} from 'pinia';
-
 import {defaultTheme, darkTheme} from '../lib/theme'
+import {setStatusBarStyle} from "../lib/utils";
+import {computed, ref} from "vue";
 
-let isDark = false,
-    windowInfo = null,
-    systemInfo = null
+export const usePetalUiStore = defineStore('petal_ui', () => {
 
-export const usePetalUiStore = defineStore('petal_ui', {
-    state: () => {
-        if (windowInfo === null) {
-            windowInfo = uni.getWindowInfo()
-            systemInfo = uni.getSystemInfoSync()
+    // 是否开启深色模式
+    const dark = ref(false)
 
-            // 监听窗口变化
-            uni.onWindowResize(() => {
-                windowInfo = uni.getWindowInfo()
-            })
-        }
-        return {
-            dark: isDark,
-            windowInfo,
-            systemInfo
-        }
-    },
-    actions: {
-        setTheme(values, isDark = false) {
-            //TODO: 这里设置主题颜色后getters的theme并不会变化
-            if (isDark) {
-                for (let i in values) {
-                    darkTheme[i] = values[i];
-                }
-            } else {
-                for (let i in values) {
-                    defaultTheme[i] = values[i];
-                }
-            }
-        },
-    },
-    getters: {
-        theme: (state) => {
-            return state.dark ? darkTheme : defaultTheme;
-        }
+    let windowInfo = ref(uni.getWindowInfo()),
+        systemInfo = ref(uni.getSystemInfoSync())
+    // 监听窗口变化
+    uni.onWindowResize(() => {
+        windowInfo.value = uni.getWindowInfo()
+    })
+
+    // 设置是否开启深色模式
+    const setDark = (val) => {
+        dark.value = Boolean(val);
     }
-});
+
+    // 主题颜色
+    const theme = computed(() => dark.value ? darkTheme : defaultTheme)
+
+    return {
+        dark, theme, windowInfo, systemInfo,
+        setDark
+    }
+})
